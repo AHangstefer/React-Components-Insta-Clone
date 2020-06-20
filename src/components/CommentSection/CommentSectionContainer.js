d2c320ca60a9f9df037d625ed9d03c18a22307ad
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-06-19 21:58:39 -0400

    working comment Section Container

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,20 +3,23 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
-import dummyData from "../../dummy-data.js";
+//import dummyData from "../../dummy-data.js";
 
 
 const CommentSection = props => {
-   const [data, setData] = useState(dummyData);
-   console.log(data);
-  console.log ("These are props from CommentSection", props);
+   //console.log ("These are props from CommentSection", props);
 
   // Add state for the comments
+  //const [points, setPoints]= useState(props.post)
 
   return (
     <div>
+      {/* {data.map(c=> (
+        <Comment key={c.id} post=[c] />
+      ))} */}
+
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {/*<CommentInput />*/}
 
     </div>
   );
